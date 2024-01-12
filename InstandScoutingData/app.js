3e367caee5ca57969e868111cdf0f290ed015859
Ky Duyen <daonguyenk06@gmail.com>
2024-01-11 18:32:50 -0600

    crescendo pic uploaded

--- InstandScoutingData/app.js
+++ InstandScoutingData/app.js
@@ -4,13 +4,8 @@ import { ref, push, onValue, update, remove } from "https://www.gstatic.com/fire
 //(import *) lets you import everything from that file
 import * as teamDatabase from "../firebase.js";
 
-import * as points from "/InstandScoutingForm/app.js";
-console.log(points.contributedPoints);
-
 //**By wrapping the code inside the DOMContentLoaded event listener, you ensure that the code will only run when the DOM is ready.
 document.addEventListener("DOMContentLoaded", function() {
-
-    //TODO: Keep comments hidden from 'guest' user
 
     
 });
