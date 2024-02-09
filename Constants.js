fae509190e4c97438f4a15eb836caf3f1cf373a8
Ky Duyen <daonguyenk06@gmail.com>
2024-02-08 19:59:46 -0600

    code cleanup

--- Constants.js
+++ Constants.js
@@ -6,3 +6,7 @@ const event_key = `mndu`; //Found on 'apiDoc - Blue Alliance' OR 'Scoutradioz'
 const baseUrl = 'https://www.thebluealliance.com/api/v3'; // Adjust the base URL based on the TBA API version
 const path = `/event/${year}${event_key}/matches`;
 export const url = `${baseUrl}${path}`;
+
+export function fetchAttendingTeams() {
+    
+}
