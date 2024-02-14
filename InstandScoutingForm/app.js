15845aaab0e83a85e1b254da894a230433ae8d77
Ky Duyen <daonguyenk06@gmail.com>
2024-02-13 18:49:42 -0600

    redesign; added team options

--- InstandScoutingForm/app.js
+++ InstandScoutingForm/app.js
@@ -5,7 +5,37 @@ import { getDatabase, ref, push, onValue, update, set, remove, child} from "http
 import * as constants from "../Constants.js";
 
 //**By wrapping the code inside the DOMContentLoaded event listener, you ensure that the code will only run when the DOM is ready.
-document.addEventListener("DOMContentLoaded", function() {
-    
+document.addEventListener("DOMContentLoaded", function() {  
+const appSettings = {
+    databaseURL: "https://scoutingapp-e16c4-default-rtdb.firebaseio.com/"
+}
+
+// Initialize Firebase
+const app = initializeApp(appSettings);
+
+//Connects database to app
+const database = getDatabase(app); //Realtime-database
+
+//Firebase references
+const teamInfo = ref(database, "teamInfo");
+
+//HTML Elements
+const teamInfo_table = document.getElementsByClassName("teamInfo");
+const auto_table = document.getElementsByClassName("auto");
+const teleop_table = document.getElementsByClassName("teleop");
+const endgame_table = document.getElementsByClassName("endgame");
+const selectTeam = document.getElementById("selectTeam");
+const matchNum = document.getElementById("matchNum");
+const teamNum = document.getElementById("teamNum");
+
+
+onValue(teamInfo, function(snapshot) {
+    const teamInfo_Array = Object.values(snapshot.val());
+    let teamList = [];
+    for(let i = 0; i < teamInfo_Array.length; i++) {
+        teamList.push(teamInfo_Array[i].team_number);
+        selectTeam.innerHTML += `<option value="" id="selectTeam_${i}">${teamInfo_Array[i].team_number}</option>`
+    }
+});
 
 });
