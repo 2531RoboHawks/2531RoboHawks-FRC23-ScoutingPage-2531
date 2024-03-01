870872c29ce67c38858398f7b5262ec80da583f5
Ky Duyen <98281051+daonguyenk06@users.noreply.github.com>
2024-03-01 13:15:46 -0600

    Update app.js

--- QualificationMatchesSchedule/app.js
+++ QualificationMatchesSchedule/app.js
@@ -147,7 +147,7 @@ onValue(matchesData, function(snapshot) {
             document.getElementById(`matchElement_${time}`).style.backgroundColor = 'purple';
             document.getElementById(`matchElement_${time}`).style.color = 'gold';
                 for(let i = 0; i < qualData_firebase.length; i++) {
-                    if(typeof actualTimeArray[i] === "number") {
+                    if(typeof actualTimeArray[i] === "number" && actualTimeArray[i] > 0) {
                         //Check if match won
                         if(qualData_firebase[time].winning_alliance == 'red') {
                             document.getElementById(`timeElement_${time}`).innerHTML = 'Won';
@@ -185,7 +185,7 @@ onValue(matchesData, function(snapshot) {
             document.getElementById(`matchElement_${time}`).style.backgroundColor = 'purple';
             document.getElementById(`matchElement_${time}`).style.color = 'gold';
                 for(let i = 0; i < qualData_firebase.length; i++) {
-                    if(typeof actualTimeArray[i] === "number") {
+                    if(typeof actualTimeArray[i] === "number" && actualTimeArray[i] > 0) {
                         // Check if match won
                         if(qualData_firebase[time].winning_alliance == 'blue') {
                             document.getElementById(`timeElement_${time}`).innerHTML = 'Won';
