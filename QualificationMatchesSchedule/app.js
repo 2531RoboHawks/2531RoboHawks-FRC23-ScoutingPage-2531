570c2f3af4b3ccd5a8344a781689d0fdf54b67b2
Ky Duyen <daonguyenk06@gmail.com>
2024-03-01 23:00:01 -0600

    Moved Constants.js to a folder

--- QualificationMatchesSchedule/app.js
+++ QualificationMatchesSchedule/app.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
 import { getDatabase, ref, push, onValue, update, set, remove, child} from "https://www.gstatic.com/firebasejs/10.0.0/firebase-database.js";
 
-import * as constants from "../Constants.js";
+import * as constants from "../Constants/API.js";
 
 //**By wrapping the code inside the DOMContentLoaded event listener, you ensure that the code will only run when the DOM is ready.
 document.addEventListener("DOMContentLoaded", function() {
