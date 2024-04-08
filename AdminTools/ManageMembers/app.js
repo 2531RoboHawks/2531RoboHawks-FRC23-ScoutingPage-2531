8b3179662d25e70bbc435b17bb5a656a1a052456
Ky Duyen <daonguyenk06@gmail.com>
2024-04-07 19:23:36 -0500

    Created Manage Members page

---
+++ AdminTools/ManageMembers/app.js
@@ -0,0 +1,45 @@
+//Firebase imports
+import { database } from "../../Constants/firebaseConfig.js";
+import { getDatabase, ref, push, onValue, update, set, remove, child} from "https://www.gstatic.com/firebasejs/10.0.0/firebase-database.js";
+
+import { match_layout } from "../../Constants/layout.js";
+import { inputTypes } from "../../Constants/layout.js";
+
+//**By wrapping the code inside the DOMContentLoaded event listener, you ensure that the code will only run when the DOM is ready.
+document.addEventListener("DOMContentLoaded", function() {  
+
+//HTML elements
+const menuIcon = document.getElementById('menu-icon');
+const allSections = document.querySelectorAll('section');
+const sidebar = document.getElementById('section');
+
+//Show sidebar
+menuIcon.addEventListener('click', function(event) {
+    event.stopPropagation(); // Prevent the click event from propagating to the body
+    // Toggle the sidebar's visibility by changing its left position
+    sidebar.style.left = '0'; // Show sidebar
+});
+
+
+// Add click event listener to custom links
+document.querySelectorAll('.custom-link').forEach(link => {
+    link.addEventListener('click', function() {
+        const href = this.getAttribute('data-href');
+        if (href) {
+            // Navigate to the specified URL
+            window.location.href = href;
+        }
+    });
+});
+
+// Close all sections if a click event occurs outside of it
+document.addEventListener('click', function(event) {
+    // Check if the clicked element is within a section
+    if (!event.target.closest('section')) {
+        for (let i = 0; i < allSections.length; i++) {
+            allSections[i].style.left = '-100%'; // Hide section
+        }
+    }
+});
+
+});
