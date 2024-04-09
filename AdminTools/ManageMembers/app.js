18cd4c90f29dde2606fc3ff21bdc724779a08079
Ky Duyen <daonguyenk06@gmail.com>
2024-04-08 23:05:12 -0500

    continue designing manage members page

--- AdminTools/ManageMembers/app.js
+++ AdminTools/ManageMembers/app.js
@@ -2,8 +2,6 @@
 import { database } from "../../Constants/firebaseConfig.js";
 import { getDatabase, ref, push, onValue, update, set, remove, child} from "https://www.gstatic.com/firebasejs/10.0.0/firebase-database.js";
 
-import { match_layout } from "../../Constants/layout.js";
-import { inputTypes } from "../../Constants/layout.js";
 
 //**By wrapping the code inside the DOMContentLoaded event listener, you ensure that the code will only run when the DOM is ready.
 document.addEventListener("DOMContentLoaded", function() {  
